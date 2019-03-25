ebf9d0d06604bb1b93dea19dc4e9756c13918309
Martinlinux1 <martin.robomaze@yandex.com>
2019-03-25 15:24:16 +0100

    Motors are working.

--- FireFighterSensorInterface/src/Motors.cpp
+++ FireFighterSensorInterface/src/Motors.cpp
@@ -3,78 +3,84 @@
 Motors::Motors(short *motorPins) {
   Motors::motorPins = motorPins;
 
-  for (int i = 0; i < arrLen(Motors::motorPins); i++) {
-    ledcSetup(Motors::channels[i], Motors::frequency, LEDC_TIMER_13_BIT);
+  for (int i = 0; i < 8; i++) {
+    pinMode(Motors::motorPins[i], OUTPUT);
+    ledcSetup(Motors::channels[i], Motors::frequency, 13);
     ledcAttachPin(Motors::motorPins[i], Motors::channels[i]);
   }
+
+  digitalWrite(0, LOW);
+  digitalWrite(15, LOW);
+  digitalWrite(5, LOW);
 }
 
 void Motors::motorWrite(char motor, char direction, short speed) {
   if (motor == 'A') {
     if (direction == 'F') {
       Motors::ledcAnalogWrite(Motors::channels[0], speed);
-      Motors::ledcAnalogWrite(Motors::channels[0], 0);
+      Motors::ledcAnalogWrite(Motors::channels[1], 0);
     }
     else if (direction == 'B') {
-      ledcWrite(Motors::channels[0], 0);
-      ledcWrite(Motors::channels[1], speed);
+      Motors::ledcAnalogWrite(Motors::channels[0], 0);
+      Motors::ledcAnalogWrite(Motors::channels[1], speed);
     }
   }
 
   else if (motor == 'B') {
     if (direction == 'F') {
-      ledcWrite(Motors::channels[2], speed);
-      ledcWrite(Motors::channels[3], 0);
+      Motors::ledcAnalogWrite(Motors::channels[2], speed);
+      Motors::ledcAnalogWrite(Motors::channels[3], 0);
     }
     else if (direction == 'B') {
-      ledcWrite(Motors::channels[2], 0);
-      ledcWrite(Motors::channels[3], speed);
+      Motors::ledcAnalogWrite(Motors::channels[2], 0);
+      Motors::ledcAnalogWrite(Motors::channels[3], speed);
     }
   }
 
   else if (motor == 'C') {
     if (direction == 'F') {
-      ledcWrite(Motors::channels[4], speed);
-      ledcWrite(Motors::channels[5], 0);
+      Motors::ledcAnalogWrite(Motors::channels[4], speed);
+      Motors::ledcAnalogWrite(Motors::channels[5], 0);
     }
     else if (direction == 'B') {
-      ledcWrite(Motors::channels[4], 0);
-      ledcWrite(Motors::channels[5], speed);
+      Motors::ledcAnalogWrite(Motors::channels[4], 0);
+      Motors::ledcAnalogWrite(Motors::channels[5], speed);
     }
   }
 
   else if (motor == 'D') {
     if (direction == 'F') {
-      ledcWrite(Motors::channels[6], speed);
-      ledcWrite(Motors::channels[7], 0);
+      Motors::ledcAnalogWrite(Motors::channels[6], speed);
+      Motors::ledcAnalogWrite(Motors::channels[7], 0);
     }
     else if (direction == 'B') {
-      ledcWrite(Motors::channels[6], 0);
-      ledcWrite(Motors::channels[7], speed);
+      Motors::ledcAnalogWrite(Motors::channels[6], 0);
+      Motors::ledcAnalogWrite(Motors::channels[7], speed);
     }
   }
 }
 
 void Motors::forward(String motors, short speed) {
-  char *motor1;
-  char *motor2;
+  char motorsC[2];
 
-  motors.substring(0, 1).toCharArray(motor1, 1);
-  motors.substring(1).toCharArray(motor2, 1);
+  motors.toCharArray(motorsC, 2);
 
-  motorWrite(motor1[0], 'F', speed);
-  motorWrite(motor2[0], 'F', speed);
+  motorWrite(motorsC[0], 'F', speed);
+  motorWrite(motorsC[1], 'F', speed);
 }
 
 void Motors::backward(String motors, short speed) {
-  char *motor1;
-  char *motor2;
+  char motorsC[5];
 
-  motors.substring(0, 1).toCharArray(motor1, 1);
-  motors.substring(1).toCharArray(motor2, 1);
+  Serial.println(motors);
 
-  motorWrite(motor1[0], 'B', speed);
-  motorWrite(motor2[0], 'B', speed);
+  motors.toCharArray(motorsC, 5);
+
+  Serial.println(motorsC[0]);
+  Serial.println(motorsC[1]);
+
+  motorWrite(motorsC[0], 'B', speed);
+  motorWrite(motorsC[1], 'B', speed);
 }
 
 void Motors::turn(char direction, short speed) {
@@ -92,7 +98,7 @@ void Motors::turn(char direction, short speed) {
   }
 }
 
-void Motors::ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
+void Motors::ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax) {
   // calculate duty, 8191 from 2 ^ 13 - 1
   uint32_t duty = (8191 / valueMax) * min(value, valueMax);
 
--- FireFighterSensorInterface/src/motrtsTest.cpp
+++ FireFighterSensorInterface/src/motrtsTest.cpp
@@ -6,9 +6,17 @@ short motorPins[8] = {19, 18, 5, 17, 16, 4, 0, 2};
 Motors motors(motorPins);
 
 void setup() {
-
+  Serial.begin(9600);
+  pinMode(5, OUTPUT);
+  digitalWrite(5, LOW);
+  pinMode(0, OUTPUT);
+  digitalWrite(0, LOW);
+  pinMode(15, OUTPUT);
+  digitalWrite(15, LOW);
 }
 
 void loop() {
   motors.forward("AB", 255);
+  motors.motorWrite('C', 'F', 127);
+  motors.motorWrite('D', 'B', 127);
 }
