627c1161bccf001cdb1478073601e8f86a2975f5
Martinlinux1 <matko.micuch@gmail.com>
2019-07-30 21:58:26 +0200

    Created a fire detection program.

--- pythonApp/fireFighter/main.py
+++ pythonApp/fireFighter/main.py
@@ -1,6 +1,7 @@
 import serial
 import communicationHandler
 import motorController
+from pyusb2fir import USB2FIR
 from time import sleep
 
 serialLink = serial.Serial("/dev/ttyUSB0", 115200, timeout=0.05)
@@ -9,29 +10,52 @@ commHandler = communicationHandler.CommunicationHandler(serialLink)
 
 motors = motorController.MotorHandler(commHandler)
 
+base_speed = 255
+
+value_black = 500
+
+thermal_camera = USB2FIR()
+
+sleep(2)
+
 while True:
-    commHandler.write_motor('A', 'F', 255)
-    commHandler.write_motor('B', 'F', 255)
+    temperatures_raw_0 = thermal_camera.initializeFrame()
+    temperatures_raw_1 = thermal_camera.initializeFrame()
+    temperatures_raw = []
 
-    sleep(5)
+    thermal_camera.updateFrame(temperatures_raw_0)
+    thermal_camera.updateFrame(temperatures_raw_1)
 
-    commHandler.write_motor('A', 'B', 255)
-    commHandler.write_motor('B', 'B', 255)
+    for i in range(768):
+        temperatures_raw.append(temperatures_raw_0[i] + temperatures_raw_1[i])
 
-    sleep(5)
+    index = 0
+    temps_row = []
+    temps = []
+    for i in range(24):
+        for j in range(32):
+            temps_row.append(temperatures_raw[index])
+            index += 1
+        temps.append(temps_row)
 
-    commHandler.write_motor('A', 'B', 0)
-    commHandler.write_motor('B', 'B', 0)
+    for i in range(24):
+        for j in range(32):
+            if temps[i][j] > 50:
+                print("Fire on(x, y): ", (j, i))
 
-    commHandler.write_motor('C', 'F', 255)
-    commHandler.write_motor('D', 'F', 255)
+                angle_x = i * (120 / 32) - 60
+                angle_y = j * (75 / 24) - 37.5
 
-    sleep(5)
+                if angle_x < 0:
+                    while angle_x < -5:
+                        motors.turn(direction='L', speed=base_speed)
+                elif angle_x > 0:
+                    while angle_x > 5:
+                        motors.turn(direction='L', speed=base_speed)
 
-    commHandler.write_motor('C', 'B', 255)
-    commHandler.write_motor('D', 'B', 255)
+                motors.brake()
 
-    sleep(5)
+                while commHandler.get_light_sensor_data(0) > value_black:
+                    motors.forward('AB', base_speed)
+                motors.brake()
 
-    commHandler.write_motor('C', 'B', 0)
-    commHandler.write_motor('D', 'B', 0)
