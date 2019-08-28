d8be0af6c3cb0f36edaf865a38221dedb53ebac8
Martinlinux1 <matko.micuch@gmail.com>
2019-08-28 15:40:27 +0200

    Removed template fire recognition, added Haar cascade fire detection.

--- pythonApp/fireFighter/opencvFireDetection.py
+++ pythonApp/fireFighter/opencvFireDetection.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('fire_1.JPG')
+image = cv2.imread('fire_4.jpg')
 cv2.imshow('image_original', image)
 
 # image = cv2.resize(image, (1366, 768))
@@ -14,22 +14,21 @@ lower = np.array(lower, dtype="uint8")
 upper = np.array(upper, dtype="uint8")
 mask = cv2.inRange(imageHSV, lower, upper)
 
-output = cv2.bitwise_and(image, imageHSV, mask=mask)
-cv2.imshow('mask', mask)
+color_output = cv2.bitwise_and(image, imageHSV, mask=mask)
 
-fire_gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
-template = cv2.imread('fire_template.png', 0)
-w, h = template.shape[::-1]
+cv2.imshow('mask', color_output)
 
-res = cv2.matchTemplate(fire_gray, template, cv2.TM_CCORR)
-print(res)
-threshold = 0.8
-loc = np.where(res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-    print(pt)
+fire_cascade = cv2.CascadeClassifier('fire_cascade.xml')
 
-cv2.imshow('fire', image)
+color_filter_gray = cv2.cvtColor(color_output, cv2.COLOR_BGR2GRAY)
+
+fires = fire_cascade.detectMultiScale(color_filter_gray)
+
+for (x, y, w, h) in fires:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
+
+
+cv2.imshow('img', image)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
