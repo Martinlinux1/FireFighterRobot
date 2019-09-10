37bd2097f593d35e972d4ea739d2df8dffd1c114
Martinlinux1 <matko.micuch@gmail.com>
2019-09-10 20:11:04 +0200

    Line avoiding logic changed - no turning when back sensors found line.

--- pythonApp/fireFighter/main.py
+++ pythonApp/fireFighter/main.py
@@ -131,41 +131,37 @@ while True:
         else:
             line = is_line()
             print(line)
-            if line:
-                if 6 in line and 5 in line and 4 in line:                   # Left downer corner.
-                    motors.turn(45, baseSpeed)
-                    motors.forward(baseSpeed)
-                elif 2 in line and 3 in line and 4 in line:                 # Right downer corner.
-                    motors.turn(-45, baseSpeed)
-                elif 0 in line and 7 in line and 6 in line:                 # Left upper corner.
-                    motors.turn(135, baseSpeed)
-                elif 0 in line and 1 in line and 2 in line:                 # Right upper corner.
-                    motors.turn(-135, baseSpeed)
-                elif 6 in line and (7 in line or 5 in line):                # Line on the left.
-                    motors.turn(90, baseSpeed)
-                elif 0 in line and (7 in line or 1 in line):                # Line on the right.
-                    motors.turn(-90, baseSpeed)
-                elif 4 in line and (5 in line or 3 in line):                # Line on the back.
-                    motors.turn(180, baseSpeed)
-                elif 2 in line and (1 in line or 3 in line):                # Line on the front.
-                    motors.turn(180, baseSpeed)
-                else:                                                       # One sensor detected the line.
-                    if 1 in line:
-                        motors.backward(baseSpeed)
-                        sleep(0.3)
-                        motors.turn(-60.0, baseSpeed)
-                    elif 3 in line:
-                        motors.forward(baseSpeed)
-                        sleep(0.3)
-                        motors.turn(-60.0, baseSpeed)
-                    elif 5 in line:
-                        motors.forward(baseSpeed)
-                        sleep(0.3)
-                        motors.turn(60.0, baseSpeed)
-                    elif 7 in line:
-                        motors.backward(baseSpeed)
-                        sleep(0.3)
-                        motors.turn(60.0, baseSpeed)
+
+            if 6 in line and 5 in line and 4 in line:                   # Left downer corner.
+                motors.turn(45, baseSpeed)
+                motors.forward(baseSpeed)
+            elif 2 in line and 3 in line and 4 in line:                 # Right downer corner.
+                motors.turn(-45, baseSpeed)
+            elif 0 in line and 7 in line and 6 in line:                 # Left upper corner.
+                motors.turn(135, baseSpeed)
+            elif 0 in line and 1 in line and 2 in line:                 # Right upper corner.
+                motors.turn(-135, baseSpeed)
+            elif 6 in line and (7 in line or 5 in line):                # Line on the left.
+                motors.turn(90, baseSpeed)
+            elif 0 in line and (7 in line or 1 in line):                # Line on the right.
+                motors.turn(-90, baseSpeed)
+            elif 4 in line and (5 in line or 3 in line):                # Line on the back.
+                motors.turn(180, baseSpeed)
+            elif 2 in line and (1 in line or 3 in line):                # Line on the front.
+                motors.turn(180, baseSpeed)
+
+            if 1 in line:
+                motors.backward(baseSpeed)
+                sleep(0.3)
+                motors.turn(-60.0, baseSpeed)
+            elif 3 or 5 in line:
+                motors.forward(baseSpeed)
+                sleep(0.2)
+            elif 7 in line:
+                motors.backward(baseSpeed)
+                sleep(0.3)
+                motors.turn(60.0, baseSpeed)
+
             else:
                 motors.forward(baseSpeed)
 
