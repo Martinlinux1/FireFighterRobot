b4eacfedca0884ba3ef842ffab93e65953475568
Martinlinux1 <matko.micuch@gmail.com>
2019-09-11 21:35:10 +0200

    Changed line avoiding algorithm.

--- pythonApp/fireFighter/main.py
+++ pythonApp/fireFighter/main.py
@@ -59,7 +59,7 @@ motors = motorController.MotorController(commHandler)
 
 baseSpeed = 150
 
-lightSensorsBlack = 600
+lightSensorsBlack = 1500
 lightSensorsWhite = 250
 
 t.start()
@@ -111,37 +111,40 @@ while True:
             line = is_line()
             print(line)
 
-            # if 6 in line and 5 in line and 4 in line:                   # Left downer corner.
-            #     motors.turn(45, baseSpeed)
-            #     motors.forward(baseSpeed)
-            # elif 2 in line and 3 in line and 4 in line:                 # Right downer corner.
-            #     motors.turn(-45, baseSpeed)
-            # elif 0 in line and 7 in line and 6 in line:                 # Left upper corner.
-            #     motors.turn(135, baseSpeed)
-            # elif 0 in line and 1 in line and 2 in line:                 # Right upper corner.
-            #     motors.turn(-135, baseSpeed)
-            # elif 6 in line and (7 in line or 5 in line):                # Line on the left.
-            #     motors.turn(90, baseSpeed)
-            # elif 0 in line and (7 in line or 1 in line):                # Line on the right.
-            #     motors.turn(-90, baseSpeed)
-            # elif 4 in line and (5 in line or 3 in line):                # Line on the back.
-            #     motors.turn(180, baseSpeed)
-            # elif 2 in line and (1 in line or 3 in line):                # Line on the front.
+            if 6 in line and 5 in line and 4 in line:                   # Left downer corner.
+                motors.turn(45, baseSpeed)
+                motors.forward(baseSpeed)
+            elif 2 in line and 3 in line and 4 in line:                 # Right downer corner.
+                motors.turn(-45, baseSpeed)
+            elif 0 in line and 7 in line and 6 in line:                 # Left upper corner.
+                motors.turn(135, baseSpeed)
+            elif 0 in line and 1 in line and 2 in line:                 # Right upper corner.
+                motors.turn(-135, baseSpeed)
+            elif 6 in line and (7 in line or 5 in line):                # Line on the left.
+                motors.turn(90, baseSpeed)
+            elif 0 in line and (7 in line or 1 in line):                # Line on the right.
+                motors.turn(-90, baseSpeed)
+            elif 4 in line and (5 in line or 3 in line):                # Line on the back.
+                motors.turn(180, baseSpeed)
+            elif 2 in line and (1 in line or 3 in line):                # Line on the front.
             #     motors.turn(180, baseSpeed)
 
             if 1 in line:
-                # motors.backward(baseSpeed)
-                # sleep(0.3)
+                motors.backward(baseSpeed)
+                sleep(0.3)
                 motors.brake()
+                print('right')
                 motors.turn(-60.0, baseSpeed)
+            elif 7 in line:
+                motors.backward(baseSpeed)
+                sleep(0.3)
+                motors.brake()
+                print('left')
+                motors.turn(60.0, baseSpeed)
             elif 3 or 5 in line:
                 motors.forward(baseSpeed)
                 sleep(0.2)
-            elif 7 in line:
-                # motors.backward(baseSpeed)
-                # sleep(0.3)
-                motors.brake()
-                motors.turn(60.0, baseSpeed)
+
 
             else:
                 motors.forward(baseSpeed)
