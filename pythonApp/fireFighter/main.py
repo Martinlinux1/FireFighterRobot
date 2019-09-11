59277ec8f35d3604da99d6b9318fcea70b24ffef
Martinlinux1 <matko.micuch@gmail.com>
2019-09-11 21:05:15 +0200

    Adjusted sensor black value.

--- pythonApp/fireFighter/main.py
+++ pythonApp/fireFighter/main.py
@@ -31,7 +31,9 @@ class CameraFetcher(threading.Thread):
 def is_line():
     on_line_sensors = []
     for i in range(8):
-        if commHandler.get_light_sensor_data(i) > lightSensorsBlack:
+        light_data = commHandler.get_light_sensor_data(i)
+        print(light_data)
+        if light_data > lightSensorsBlack:
             on_line_sensors.append(i)
 
     return on_line_sensors
@@ -57,7 +59,7 @@ motors = motorController.MotorController(commHandler)
 
 baseSpeed = 150
 
-lightSensorsBlack = 2000
+lightSensorsBlack = 600
 lightSensorsWhite = 250
 
 t.start()
@@ -109,34 +111,36 @@ while True:
             line = is_line()
             print(line)
 
-            if 6 in line and 5 in line and 4 in line:                   # Left downer corner.
-                motors.turn(45, baseSpeed)
-                motors.forward(baseSpeed)
-            elif 2 in line and 3 in line and 4 in line:                 # Right downer corner.
-                motors.turn(-45, baseSpeed)
-            elif 0 in line and 7 in line and 6 in line:                 # Left upper corner.
-                motors.turn(135, baseSpeed)
-            elif 0 in line and 1 in line and 2 in line:                 # Right upper corner.
-                motors.turn(-135, baseSpeed)
-            elif 6 in line and (7 in line or 5 in line):                # Line on the left.
-                motors.turn(90, baseSpeed)
-            elif 0 in line and (7 in line or 1 in line):                # Line on the right.
-                motors.turn(-90, baseSpeed)
-            elif 4 in line and (5 in line or 3 in line):                # Line on the back.
-                motors.turn(180, baseSpeed)
-            elif 2 in line and (1 in line or 3 in line):                # Line on the front.
-                motors.turn(180, baseSpeed)
+            # if 6 in line and 5 in line and 4 in line:                   # Left downer corner.
+            #     motors.turn(45, baseSpeed)
+            #     motors.forward(baseSpeed)
+            # elif 2 in line and 3 in line and 4 in line:                 # Right downer corner.
+            #     motors.turn(-45, baseSpeed)
+            # elif 0 in line and 7 in line and 6 in line:                 # Left upper corner.
+            #     motors.turn(135, baseSpeed)
+            # elif 0 in line and 1 in line and 2 in line:                 # Right upper corner.
+            #     motors.turn(-135, baseSpeed)
+            # elif 6 in line and (7 in line or 5 in line):                # Line on the left.
+            #     motors.turn(90, baseSpeed)
+            # elif 0 in line and (7 in line or 1 in line):                # Line on the right.
+            #     motors.turn(-90, baseSpeed)
+            # elif 4 in line and (5 in line or 3 in line):                # Line on the back.
+            #     motors.turn(180, baseSpeed)
+            # elif 2 in line and (1 in line or 3 in line):                # Line on the front.
+            #     motors.turn(180, baseSpeed)
 
             if 1 in line:
-                motors.backward(baseSpeed)
-                sleep(0.3)
+                # motors.backward(baseSpeed)
+                # sleep(0.3)
+                motors.brake()
                 motors.turn(-60.0, baseSpeed)
             elif 3 or 5 in line:
                 motors.forward(baseSpeed)
                 sleep(0.2)
             elif 7 in line:
-                motors.backward(baseSpeed)
-                sleep(0.3)
+                # motors.backward(baseSpeed)
+                # sleep(0.3)
+                motors.brake()
                 motors.turn(60.0, baseSpeed)
 
             else:
