da9d047f66ebed6d61a8632de9fd780e33a0a0aa
Martinlinux1 <matko.micuch@gmail.com>
2019-10-26 21:14:41 +0200

    Multiprocessing communication finally works

--- pythonApp/fireFighter/main.py
+++ pythonApp/fireFighter/main.py
@@ -7,34 +7,32 @@ import communicationHandler
 import motorController
 
 
-def robot_logic(sensors_comm_child_pipe, motors_comm_child_pipe):
-    motors_c = motors_comm_child_pipe.recv()
+def robot_logic(comm_child_pipe):
+    motors_c = comm_child_pipe.recv()
     while True:
-        sensor_c: communication.Communication = sensors_comm_child_pipe.recv()
-        print(sensor_c.get_light_sensors(), sensor_c.get_distance_sensors())
-        motors_c.set_motor('A', 'F', 255)
-        motors_comm_child_pipe.send(motors_c)
+        c: communication.Communication = comm_child_pipe.recv()
+        print(c.get_light_sensors(), c.get_distance_sensors())
+        c.set_motor('A', 'F', 255)
+        comm_child_pipe.send(c)
 
 
 serial_port = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.05)
 comm_handler = communicationHandler.CommunicationHandler(serial_port)
-sensors_comm: communication.Communication = communication.Communication(comm_handler)
-motors_comm: communication.Communication = communication.Communication(comm_handler)
+comm: communication.Communication = communication.Communication(comm_handler)
 
-motors = motorController.MotorController(motors_comm, 0.05)
+motors = motorController.MotorController(comm, 0.05)
 
-sensors_comm_parent, sensors_comm_child = multiprocessing.Pipe()
-motors_comm_parent, motors_comm_child = multiprocessing.Pipe()
+comm_parent, comm_child = multiprocessing.Pipe()
 
-robot_logic_process = multiprocessing.Process(target=robot_logic, args=[sensors_comm_child, motors_comm_child])
+robot_logic_process = multiprocessing.Process(target=robot_logic, args=[comm_child])
 
-motors_comm_parent.send(motors_comm)
+comm_parent.send(comm)
 
 robot_logic_process.start()
 
 while True:
-    sensors_comm.update_sensors()
-    sensors_comm_parent.send(sensors_comm)
+    comm.update_sensors()
+    comm_parent.send(comm)
 
-    motors_comm = motors_comm_parent.recv()
-    motors_comm.update_motors()
+    comm = comm_parent.recv()
+    comm.update_motors()
