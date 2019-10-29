58195eb3c6e6412cda30adf75b8a170207131dfb
Martinlinux1 <matko.micuch@gmail.com>
2019-10-29 22:03:23 +0100

    Added line and obstacle avoiding logic.

--- pythonApp/fireFighter/main.py
+++ pythonApp/fireFighter/main.py
@@ -1,4 +1,5 @@
 import multiprocessing
+from time import sleep
 
 import serial
 
@@ -7,7 +8,7 @@ import communicationHandler
 import motorController
 
 
-def robot_logic(comm_child_pipe):
+def robot_data_handler(comm_child_pipe):
     c: communication.Communication = comm_child_pipe.recv()
     while True:
         c.update_sensors()
@@ -26,10 +27,31 @@ motors = motorController.MotorController(comm, 0.05)
 
 comm_parent, comm_child = multiprocessing.Pipe()
 
-robot_logic_process = multiprocessing.Process(target=robot_logic, args=[comm_child])
+robot_logic_process = multiprocessing.Process(target=robot_data_handler, args=[comm_child])
 comm_parent.send(comm)
 
 robot_logic_process.start()
+
+base_speed = 150
+
+
+def is_line(line_sensors_data):
+    on_line_sensors = []
+    for i in range(8):
+        if line_sensors_data[i] > 2000:
+            on_line_sensors.append(i)
+
+    return on_line_sensors
+
+
+def is_obstacle(distance_sensors_data):
+    sensors_detected = []
+
+    for i in range(5):
+        if not distance_sensors_data[i]:
+            sensors_detected.append(i)
+
+    return sensors_detected
 
 
 def turn(angle, speed):
@@ -43,6 +65,7 @@ def turn(angle, speed):
 
     while True:
         comm = comm_parent.recv()
+        motors = motorController.MotorController(comm, 0.05)
         robot_angle = comm.get_imu_sensor()
         print(robot_angle)
 
@@ -60,11 +83,49 @@ def turn(angle, speed):
             comm_parent.send(comm)
             return
 
+
 while True:
     comm = comm_parent.recv()
+    motors = motorController.MotorController(comm, 0.05)
+    turn(-90, 200)
 
     light_sensors = comm.get_light_sensors()
     distance_sensors = comm.get_distance_sensors()
     imu_sensor = comm.get_imu_sensor()
 
-    if light_sensors:
+    sensors_on_line = is_line(light_sensors)
+    obstacles = is_obstacle(distance_sensors)
+
+    if sensors_on_line:
+        if (0 or 7) in sensors_on_line:
+            motors.backward(base_speed)
+            comm_parent.send(comm)
+            sleep(0.1)
+            turn(60, base_speed)
+        elif 1 in sensors_on_line:
+            motors.backward(base_speed)
+            comm_parent.send(comm)
+            sleep(0.1)
+            turn(-60, base_speed)
+
+    elif obstacles:
+        if 0 in obstacles:
+            motors.backward(base_speed)
+            comm_parent.send(comm)
+            sleep(0.1)
+            turn(-90, base_speed)
+        elif 1 in obstacles:
+            motors.backward(base_speed)
+            comm_parent.send(comm)
+            sleep(0.1)
+            turn(-45, base_speed)
+        elif 3 in obstacles:
+            motors.backward(base_speed)
+            comm_parent.send(comm)
+            sleep(0.1)
+            turn(45, base_speed)
+
+    else:
+        motors.forward(base_speed)
+
+    comm_parent.send(comm)
