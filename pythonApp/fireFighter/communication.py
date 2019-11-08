f42d3ea7b66818ff8e3a7cc210fefb1e203f9a60
Martinlinux1 <matko.micuch@gmail.com>
2019-11-08 22:27:58 +0100

    Moved communication pipe to communication class.

--- pythonApp/fireFighter/communication.py
+++ pythonApp/fireFighter/communication.py
@@ -1,12 +1,15 @@
+from multiprocessing import Pipe
+
 import numpy as np
 
 
 class Communication:
-    def __init__(self, communication_handler, sensors_reader, motor_writer):
-        self._communication_handler = communication_handler
-
+    def __init__(self, sensors_reader, motor_writer):
         self._sensors_reader = sensors_reader
         self._motor_writer = motor_writer
+
+        self._sensors_pipe_parent, self._sensors_pipe_child = Pipe()
+        self._motors_pipe_parent, self._motors_pipe_child = Pipe()
 
         self._light_sensors = np.int(8)
         self._distance_sensors = np.int(5)
@@ -24,26 +27,24 @@ class Communication:
         self._distance_sensors = sensors_data[1][1]
         self._imu_sensor = sensors_data[2][1]
 
-        return self._light_sensors, self._distance_sensors, self._imu_sensor
+        self._sensors_pipe_parent.send([self._light_sensors, self._distance_sensors, self._imu_sensor])
 
-    def get_light_sensors(self):
-        return self._light_sensors
-
-    def get_distance_sensors(self):
-        return self._distance_sensors
-
-    def get_imu_sensor(self):
-        return self._imu_sensor
+    def get_sensors(self):
+        if self._sensors_pipe_child.poll():
+            sensors = self._sensors_pipe_child.recv()
+            return sensors
 
     def update_motors(self):
-        if self._motorA:
-            self._motor_writer.write_motor('A', self._motorA[0], self._motorA[1])
-        if self._motorB:
-            self._motor_writer.write_motor('B', self._motorB[0], self._motorB[1])
-        if self._motorC:
-            self._motor_writer.write_motor('C', self._motorC[0], self._motorC[1])
-        if self._motorD:
-            self._motor_writer.write_motor('D', self._motorD[0], self._motorD[1])
+        motors = self._motors_pipe_child.recv()
+
+        if motors[0]:
+            self._motor_writer.write_motor('A', motors[0][0], motors[0][1])
+        if motors[1]:
+            self._motor_writer.write_motor('B', motors[1][0], motors[1][1])
+        if motors[2]:
+            self._motor_writer.write_motor('C', motors[2][0], motors[2][1])
+        if motors[3]:
+            self._motor_writer.write_motor('D', motors[3][0], motors[3][1])
 
     def set_motor(self, motor, direction, speed):
         if motor == 'A':
@@ -54,3 +55,5 @@ class Communication:
             self._motorC = [direction, speed]
         elif motor == 'D':
             self._motorD = [direction, speed]
+
+        self._motors_pipe_parent.send([self._motorA, self._motorB, self._motorC, self._motorD])
